a74285eb26bc250c9e9ab645c84ec65af6b1977b
lidongdong <lidongdongbuaa@gmail.com>
2019-11-06 17:14:40 +0900

    test

---
+++ linkedList/链表删除/fjdd.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/6 17:13
+# @Author  : LI Dongdong
+# @FileName: fjdd.py
+''''''
+'''
+题目分析
+1.要求：
+2.理解：
+3.类型：
+4.方法及方法分析：
+time complexity order: 
+space complexity order: 
+'''
+
+'''
+法
+思路：
+方法：
+边界条件：
+time complex: 
+space complex: 
+易错点：
+'''
