2c8850df85fe557f083ebbf6386c662248472c58
lidongdong <lidongdongbuaa@gmail.com>
2019-11-08 11:50:07 +0900

    191108

---
+++ linkedList/链表的归并与添加/2. Add Two Numbers.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/8 9:22
+# @Author  : LI Dongdong
+# @FileName: 2. Add Two Numbers.py
+''''''
+'''
+题目分析
+1.要求：You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+        You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+        (2 -> 4 -> 3) + (5 -> 6 -> 4) -> (7 -> 0 -> 8)
+2.理解：两个链表，数字对应的两两相加，不含前导0，即链表结束，数也就结束了；两个链表都非空
+    与369题类似，可以深化为怎么求两个整数 (3 -> 4 -> 2) + (4 -> 6 -> 5)的和，即先转置成本题这样，再转置回去
+3.类型：链表节点求和
+4.方法及方法分析：两两值相加迭代法
+time complexity order: 两两值相加迭代法 O(N)
+space complexity order:  两两值相加迭代法 O(N)
+'''
+
+'''
+两两值相加迭代法
+思路：构建新链表，同时迭代两个链表，把两个链表节点值的和构建成新链表的节点值；直到其中一个/两个链表到底
+方法: 新建链表，while l1 and l2, 两个链表节点和为新链表的节点值，若为10，则加入下一位，若为某链末尾，则另一个链表下一位加1
+边界条件：节点和为10时/节点和为10且为最后一位时
+time complex: tO(N)
+space complex: sO(N)
+易错点：迭代链表(while)时，一定不要忘了head.next = head-->首先就要添加
+    此题可以为9+9，跟369加1是不一样的，故curr.next = ListNode(l1.val + l2.val + i - 10)，不要忘记减10
+    同样的9+9，在最后，可能出现l1和l2都为None时，这个时候要新建1node添加到链表最后
+'''
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addOne(self, head):
+        curr = head
+        while head:
+            if head.val + 1 == 10:
+                head.val = 0
+                if head.next is None:
+                    head.next = ListNode(1)
+                    break
+                head = head.next
+            elif head.val + 1 != 10:
+                head.val += 1
+                break
+        return curr
+
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        curr = new_head = ListNode(0)
+        i = 0
+        while l1 and l2: #tO(N), sO(N)
+            if l1.val + l2.val + i >= 10:
+                curr.next = ListNode(l1.val + l2.val + i - 10)
+                i = 1
+            else:
+                curr.next = ListNode(l1.val + l2.val + i)
+                i = 0
+            curr = curr.next
+            l1 = l1.next
+            l2 = l2.next
+
+        if i == 0:#tO(1), sO(1)
+            if l1 is None:
+                curr.next = l2
+            elif l2 is None:
+                curr.next = l1
+        elif i == 1:  #tO(N), sO(N)
+            if l1 is None and l2 is not None:
+                curr.next = self.addOne(l2)
+            elif l2 is None and l1 is not None:
+                curr.next = self.addOne(l1)
+            elif l1 is None and l2 is None:
+                curr.next = ListNode(1)
+
+        return new_head.next
+
+'''
+转化为数字法
+思路：构建新链表，同时迭代两个链表，把两个链表节点值的和构建成新链表的节点值；直到其中一个/两个链表到底
+方法: 新建链表，while l1 and l2, 两个链表节点和为新链表的节点值，若为10，则加入下一位，若为某链末尾，则另一个链表下一位加1
+边界条件：节点和为10时/节点和为10且为最后一位时
+time complex: tO(N)
+space complex: sO(N)
+易错点：迭代链表(while)时，一定不要忘了head.next = head-->首先就要添加
+    此题可以为9+9，跟369加1是不一样的，故curr.next = ListNode(l1.val + l2.val + i - 10)，不要忘记减10
+    同样的9+9，在最后，可能出现l1和l2都为None时，这个时候要新建1node添加到链表最后
+'''
--- linkedList/链表的归并与添加/369. Plus One Linked List.py
+++ linkedList/链表的归并与添加/369. Plus One Linked List.py
@@ -7,21 +7,77 @@
 '''
 题目分析
 1.要求：Given a non-negative integer represented as non-empty a singly linked list of digits, plus one to the integer.
-        You may assume the integer do not contain any leading zero, except the number 0 itself.
+        You may assume the integer do not contain any leading zero, except the number 0 itself. [1,2,3] -> [1,2,4]
         The digits are stored such that the most significant digit is at the head of the list.
-2.理解：给了我们一个链表，用来模拟一个三位数，表头是高位，现在让我们进行加1运算
-3.类型：
-4.方法及方法分析：
-time complexity order: 
-space complexity order: 
+2.理解：给了我们一个链表，用来模拟一个多位数，表头是高位，现在让我们进行加1运算 
+3.类型：链表节点求和
+4.方法及方法分析：转化为数字法,链表转置法
+time complexity order:  链表转置法 O(N) = 转化为数字法 O(N) 
+space complexity order: 链表转置法 O(1) < 转化为数字法 O(N)
 '''
 
 '''
-法
-思路：
-方法：
+转化为数字法
+思路：把链表保存成整数，整数加1，然后再还原成链表
+方法：先求链表长度 tO(N)
+    后根据长度，决定每一个node值的位置，乘以相应的分位数；tO(N) sO(1)
+    加一；tO(1) 
+    然后根据根据之前链表的长度或长度+1来得到确认新数的长度 tO(1)
+    再用//法来确定每一位的数，同时合并成新的链表 tO(N) sO(N)
 边界条件：
-time complex: 
-space complex: 
+time complex: O(N)
+space complex: O(N)
 易错点：
 '''
+
+'''
+链表转置法
+思路：先转置链表，变成从个位到高位；从低位迭代到高位，同时对个位加1，若为10，本head.val改为0，下一位继续加1；若不为，终止迭代
+方法：先迭代整个链表进行转置；
+    i=1, 后对新链表递归，若head.val + i == 10，head.val = 0, 继续下个迭代；若head.val +i !=10, head.val = head.val+ i,终止迭代，返回头节点
+    对新链表转置，返回转置后的新链表
+边界条件：head.val = 0/最高位变为10，需要加1位数
+time complex: tO(N)
+space complex: sO(1)
+易错点：一定要curr = curr.next
+    b = ListNode(2) a = b.next a = ListNode(9) 这样时a.val = 9,但是b.next依然为None,
+    即因为python等于号 = 的含义是赋地址，故在a = ListNode(9)后，a获得了新地址，b.next还是原来老a的，故不匹配
+    只有next可以链接地址，故在curr = curr.next之前把curr.next = ListNode(1)建立好
+'''
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def plusOne(self, head: ListNode) -> ListNode:
+        new_head = None
+        while head: #tO(N)
+            next_head = head.next
+            head.next = new_head
+            new_head = head
+            head = next_head
+
+        curr = new_head
+        while curr: #max is tO(N), sO(1) while的循环是基于+1是否递进
+            if curr.val + 1 == 10:
+                curr.val = 0
+                if curr.next is None:
+                    curr.next = ListNode(1) #1 是新添加的，该结束了，不然又一轮while
+                    break
+                curr = curr.next
+            else:
+                curr.val = curr.val + 1
+                break
+
+        final_head = None
+        while new_head: #tO(N)
+            next_head = new_head.next
+            new_head.next = final_head
+            final_head = new_head
+            new_head = next_head
+
+        return final_head
+
+
+
