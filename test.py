85a43470791698d3fcf02309bd4a1c7238ab1b7b
lidongdong <lidongdongbuaa@gmail.com>
2019-11-11 07:48:20 +0900

    1111

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
+
 s='ddddd'
-a = list(s)
-print(a)
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+numb = 1234
+
+pre = new_list = ListNode(0)
+for elem in reversed(str(numb)):
+    new = ListNode(int(elem))
+    pre.next = new
+    pre = pre.next
+
+print(new_list.next.val)
