66f0e87eb69fe7ca595eb5c3e161bc6359049bdc
lidongdong <lidongdongbuaa@gmail.com>
2019-11-11 09:45:52 +0900

    1111

--- test.py
+++ test.py
@@ -1,16 +1,5 @@
-
-s='ddddd'
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-numb = 1234
-
-pre = new_list = ListNode(0)
-for elem in reversed(str(numb)):
-    new = ListNode(int(elem))
-    pre.next = new
-    pre = pre.next
-
-print(new_list.next.val)
+a = True
+if a == None:
+    print(1)
+else:
+    print(2)
