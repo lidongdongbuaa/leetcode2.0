9a34f75fc014cac7f0fab2a7b7828d7eb2199090
lidongdong <lidongdongbuaa@gmail.com>
2019-11-13 10:04:20 +0900

    1113

---
+++ linkedList/链表排序与划分/148. Sort List.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/13 9:14
+# @Author  : LI Dongdong
+# @FileName: 148. Sort List.py
+''''''
+'''
+题目分析
+1.要求：Sort a linked list in O(n log n) time using constant space complexity.
+    Input: 4->2->1->3 Output: 1->2->3->4
+    Input: -1->5->3->4->0 Output: -1->0->3->4->5
+2.理解：用堆/快速/归并 三种nlongn的排序方法在原链表上进行排序，使得space complexity 为constant，即为常数
+3.类型：
+4.方法及方法分析：
+time complexity order: 
+space complexity order: 
+'''
+
+'''
+method
+idea：
+edge case：
+method：
+time complex: 
+space complex: 
+易错点：
+'''
+
+'''
+test case
+'''
