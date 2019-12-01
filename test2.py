c2c3f853ff65aad3c06a2c4d46994aef6b74c6b8
lidongdong <lidongdongbuaa@gmail.com>
2019-12-01 11:25:47 +0900

    1201

--- test2.py
+++ test2.py
@@ -1,2 +1,6 @@
-a = [1,2,3,4,5]
-print(sum(a))
+a = [1, 2, 3, 4, 5]
+b = sorted(a, key=lambda x: x ** 2)
+random = [(2, 2), (3, 4), (4, 1), (1, 3)]
+random.sort(key= lambda x:x[0], reverse=True)
+print(b)
+print(random)
