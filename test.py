d3aaf9d21e79238f2dc07eb9115cf2982d415c95
lidongdong <lidongdongbuaa@gmail.com>
2019-12-02 11:28:41 +0900

    1202

--- test.py
+++ test.py
@@ -1,35 +1,14 @@
 class Node:
     def __init__(self, val=None, next=None):
         self.val = val
-        self.next = next
+        self.next = None
 
 class Solution:
-    def insert(self, head: 'Node', insertVal: int) -> 'Node':
-        if head is None:  # edge case, head is None
-            head = Node(insertVal)
-            head.next = head
-            return head
-
-        prev, curr = head, head.next
-        toInsert = False
-
-        while True:  # iterative node, four case of node
-            if prev.val <= insertVal <= curr.val:  # case 1/4
-                toInsert = True
-            elif prev.val > curr.val:
-                if prev.val <= insertVal or insertVal <= curr.val:  # case 2/3
-                    toInsert = True
-
-            if toInsert:
-                prev.next = Node(insertVal, curr)  # mission accomplished
-                return head
-
-            prev, curr = curr, curr.next
-            if prev == head:
-                break
-
-        prev.next = Node(insertVal, curr)  # case 5, did not insert the node in the loop
-        return head
+    def pop(self, head):
+        pop_node = head
+        head = head.next
+        pop_node.next = None
+        return pop_node.val
 
 '''
 test case
@@ -40,7 +19,7 @@ A3 = Node(1)
 
 A1.next = A2
 A2.next = A3
-A3.next = A1
+
 
 X = Solution()
-print(X.insert(A1, 2))
+print(X.pop(A1))
