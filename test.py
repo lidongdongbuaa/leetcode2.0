7aac5f9b7a07d19a8efaaa2da3840b2d4ac508c7
lidongdong <lidongdongbuaa@gmail.com>
2019-12-03 10:48:46 +0900

    1203

--- test.py
+++ test.py
@@ -1,25 +1,2 @@
-class Node:
-    def __init__(self, val=None, next=None):
-        self.val = val
-        self.next = None
-
-class Solution:
-    def pop(self, head):
-        pop_node = head
-        head = head.next
-        pop_node.next = None
-        return pop_node.val
-
-'''
-test case
-'''
-A1 = Node(3)
-A2 = Node(4)
-A3 = Node(1)
-
-A1.next = A2
-A2.next = A3
-
-
-X = Solution()
-print(X.pop(A1))
+a = {1:2, 3:4, 5:6, 7:8}
+print(a.popitem())
