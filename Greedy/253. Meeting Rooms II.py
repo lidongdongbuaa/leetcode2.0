36837d3da0085e3d8843a57d97d25cc666bf9bb9
lidongdong <lidongdongbuaa@gmail.com>
2019-12-10 11:23:48 +0900

    1210

---
+++ Greedy/253. Meeting Rooms II.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2019/12/10 8:05
+# @Author  : LI Dongdong
+# @FileName: 253. Meeting Rooms II.py
+''''''
+'''
+
+1.要求：Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+    Input: [[0, 30],[5, 10],[15, 20]]  Output: 2
+    Input: [[7,10],[2,4]] Output: 1
+    Following up NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+2.理解：calculate number of rooms that is being used at the same time
+3.类型：Greedy
+4.方法及方法分析：
+time complexity order: 
+space complexity order: 
+5.edge case: 
+    input: None? Y only one? Y repeat？ Y ordered? N value range？ >=0
+    output: 0 / int
+6. focus on time or space? Whatever
+'''
+
+'''
+idea：Greedy algorithm
+Method：
+    sort the input base on list[i][0]
+    traversal the elements, record the i-end time in stack
+        traversal  the stack, compare i+1 start time with stack elements (end time)
+            if start time < stack[i],remove the i elements of stack - 无法实现
+        then add the i-end time, and save the length of stack, compare it with former length, record the max value
+    return the max value as the min rooms we need
+time complex: O(NlogK) K is number of stack
+space complex: O(N)
+易错点：
+'''
+
+"超时，tO(N2)"
+class Solution:
+    def minMeetingRooms(self, intervals) -> int:  # input[[]], find min rooms we need
+        if intervals == []:  # edge case
+            return 0
+        if len(intervals) == 1:  # edge case
+            return 1
+
+        intervals.sort(key=lambda x: x[0])  # sort the intervals based on first value of elements
+
+        stack = []
+        stack.append(intervals[0][1])
+        output = 0
+        numb = 1
+        for i in range(1, len(intervals)):  # find the min number of rooms
+            for elem in stack:
+                if intervals[i][0] < elem:
+                    numb += 1
+            stack.append(intervals[i][1])
+            output = max(output, numb)
+            numb = 1
+
+        return output
+
+
+x = Solution()
+x.minMeetingRooms([[1, 5], [8, 9], [8, 9]])
+
+"optimized code"
+'''
+改进之处：对stack进行筛选，依旧上课的时间放入tmp中，再加入新上课时间，最后赋给stack。减少了stack的元素数量，减少了遍历时间
+易错点：tmp用完以后要清空，重置为[]
+'''
+class Solution:
+    def minMeetingRooms(self, intervals) -> int:  # input[[]], find min rooms we need
+        if intervals == []:  # edge case
+            return 0
+        if len(intervals) == 1:  # edge case
+            return 1
+
+        intervals.sort(key=lambda x: x[0])  # sort the intervals based on first value of elements
+
+        stack = []
+        stack.append(intervals[0][1])
+        tmp = []
+        output = 0
+        for i in range(1, len(intervals)):  # find the min number of rooms
+            for j in range(len(stack)):
+                if stack[j] > intervals[i][0]:
+                    tmp.append(stack[j])
+            tmp.append(intervals[i][1])
+            stack = tmp
+            output = max(output, len(stack))
+            tmp = []
+        return output
--- test.py
+++ test.py
@@ -1,96 +1,25 @@
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
 class Solution:
-    def sortList(self, head: ListNode) -> ListNode:  # sort linklist
-        if not head:  # edge case
-            return None
-        if not head.next:  # edge case
-            return head
-
-        length = self.find_len(head)  # calcu length of linklist
-
-        dummy = ListNode(0)
-        dummy.next = head
-
-        interval = 1
-        while interval < length:  # bottom - up merge sort
-            head1 = dummy.next  # head of every group to be processed
-            fake_tail = dummy
-            while head1:
-                head2 = self.split(head1, interval)  # cut group1, return next adjacent group head
-                next_head1 = self.split(head2, interval)  # cut group 1's adjacent group 2, return next adjacent group head
-                merge_start, merge_end = self.merge(head1, head2)  # merge group1 and group2
-
-                fake_tail.next = merge_start  # connect merged two group with former groups
-                fake_tail = merge_end  # reset fake_tail as the current merged groups
-                fake_tail.next = next_head1  # connect merged two group with later groups
-                head1 = next_head1  # reset head of every group to be processed
-            interval = 2 * interval
-
-        return dummy.next
-
-    def find_len(self, head):  # calculate the lenght of linklist
-        if not head:  # edge case
+    def minMeetingRooms(self, intervals) -> int:  # input[[]], find min rooms we need
+        if intervals == []:  # edge case
             return 0
-        if not head.next:  # edge case
+        if len(intervals) == 1:  # edge case
             return 1
 
-        length = 0
-        curr = head
-        while curr:  # accumulate length
-            length += 1
-            curr = curr.next
-        return length
+        intervals.sort(key=lambda x: x[0])  # sort the intervals based on first value of elements
 
-    def split(self, head, interval):  # cut the group from the linklist, return next adjacent group head
-        if not head:
-            return head
+        stack = []
+        stack.append(intervals[0][1])
+        tmp = []
+        output = 0
+        for i in range(1, len(intervals)):  # find the min number of rooms
+            for j in range(len(stack)):
+                if stack[j] > intervals[i][0]:
+                    tmp.append(stack[j])
+            tmp.append(intervals[i][1])
+            stack = tmp
+            output = max(output, len(stack))
+            tmp = []
+        return output
 
-        for _ in range(interval - 1):
-            if not head.next:  # not enough node for travel
-                break
-            head = head.next
-
-
-        tail = head.next
-        head.next = None
-        return tail
-
-    def merge(self, head1, head2):  # merge group1 and group2, return merged head and tail
-        dummy = tail = ListNode(0)
-        while head1 and head2:  # merge two group
-            if head1.val < head2.val:
-                tail.next = head1
-                head1 = head1.next
-                tail = tail.next
-            else:
-                tail.next = head2
-                head2 = head2.next
-                tail = tail.next
-
-        if head1:
-            tail.next = head1
-        if head2:
-            tail.next = head2
-
-        while tail.next:  # let tail points to the end of merged linked list
-            tail = tail.next
-        return dummy.next, tail
-
-
-A1 = ListNode(-1)
-A2 = ListNode(5)
-A3 = ListNode(3)
-A4 = ListNode(4)
-A5 = ListNode(0)
-A1.next = A2
-A2.next = A3
-A3.next = A4
-A4.next = A5
-
-X = Solution()
-X.sortList(A1)
+x = Solution()
+x.minMeetingRooms([[1, 5], [8, 9], [8, 9]])
