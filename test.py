fed81cab198a41c0ef805fccb274de21e858231e
lidongdong <lidongdongbuaa@gmail.com>
2019-12-20 12:54:55 +0900

    1220

--- test.py
+++ test.py
@@ -1,41 +1,74 @@
-class Merge:
-    def mergeArr(self, arr):  # input: list[list], k, n, merge all sorted list
-        if len(arr) == 0:  # edge case
-            return
-        if len(arr) == 1:
-            return arr[0]
+# merge sort 是先分割，再排序
+# quick sort 是先排序，再分割
 
-        length = len(arr)  # divide into groups
-        mid = length // 2
-        L = arr[:mid]
-        R = arr[mid:]
+'''
+讨论if i < j 和 i += 1 的作用:
+仅不满足x < arr[j]时：
+    if一定成立 - 有无都正常
+    要寻找比x大的数，而i += 1存在时，直接跳过arr[i],这个等于arr[j]，且明显小于x的数
+    而i += 1不存在时，重复 i += 1,再进行之后流程
+    故有无 i+= 1都正常
+仅不满足i<j时，即i=j了，ij重合:
+    if 存在， i += 1存在
+        arr[i] = arr[j] 和 i+=1 都不运行，整个程序运行结束，x赋给arr[i]
+    if 存在， i += 1不存在
+        arr[i] = arr[j] 不运行，整个程序运行结束，x赋给arr[i]
+    if 不存在，i += 1存在
+        arr[j]赋给arr[i],i = j，即arr[i]不变，i + 1，
+        而下面的又进行了arr[j]赋给arr[i]，整个程序结束，x赋给arr[i+1]
+        出错
+    if 不存在，i += 1不存在
+         arr[j]赋给arr[i],i = j，即arr[i]不变， 整个程序结束，x赋给arr[i]
 
-        self.mergeArr(L)  # merge to left
-        self.mergeArr(R)  # merge to right
 
-        i = j = k = 0
-        while i < len(L[0]) and j < len(R[0]):  # merge left and right
-            if L[0][i] < R[0][j]:
-                arr[k] = L[i]
-                k += 1
-                i += 1
+故正确的是：
+if 存在，i += 1存在
+if 存在，i += 1不存在
+if 不存在，i += 1不存在
 
-            else:
-                arr[k] = R[j]
-                k += 1
-                j += 1
 
-        while i < len(L):
-            arr[k] = L[i]
-            k += 1
+'''
+
+def AdjustArr(arr, l, r):  # return pivot position
+    i = l
+    j = r
+    x = arr[i]  # 第一个坑
+    while i < j:
+        while i < j and x < arr[j]:  # find smaller val than x
+            j -= 1
+        arr[i] = arr[j]
+        while i < j and arr[i] < x:
             i += 1
-        while j < len(R):
-            arr[k] = R[j]
-            k += 1
-            j += 1
+        arr[j] = arr[i]
+    arr[i] = x
+    return i
 
+def quicksort( arr, l, r):
+    if l < r:
+        i = AdjustArr(arr, l, r)
+        quicksort(arr, l, i)
+        quicksort(arr, i + 1,r)
+    return arr
 
+class Sort:
+    def quicksort(self, arr, l, r):
+        if len(arr) == 0 or len(arr) == 1:
+            return arr
+
+        if l < r:
+            i = l
+            j = r
+            pivot = arr[i]
+            while i < j:
+                while i < j and pivot <= arr[j]:
+                    j -= 1
+                arr[i] = arr[j]
+                while i < j and arr[i] <= pivot:
+                    i += 1
+                arr[j] = arr[i]
+            arr[i] = pivot
+            self.quicksort(arr, l, i)
+            self.quicksort(arr, i + 1, r)
         return arr
 
-x = Merge()
-print(x.mergeArr([[1, 5, 9], [2, 4, 7],[3, 8, 9]]))
+print(quicksort([9,8,7,6,5,4,3,2,1,0], 0, 9))
--- test2.py
+++ test2.py
@@ -1,6 +1,50 @@
-from heapq import heapify, heappush, nlargest
-heap = [1,3]
-heappush(heap, 2)
-print(heap)
-print(nlargest(2, heap))
+# def AdjustArr(arr, l, r):  # return pivot position
+#     i = l
+#     j = r
+#     x = arr[i]  # 第一个坑
+#     while i < j:
+#         while i < j and x < arr[j]:  # find smaller val than x
+#             j -= 1
+#         if i < j:  # 区别1
+#             arr[i] = arr[j]
+#             # i += 1  #区别2
+#         while i < j and arr[i] < x:
+#             i += 1
+#         if i < j:
+#             arr[j] = arr[i]
+#             j -= 1
+#     arr[i] = x
+#     return arr
 
+
+
+class Sort:
+    def quicksort(self, arr):
+        if len(arr) == 0 or len(arr) == 1:
+            return arr
+
+        l = 0
+        r = len(arr) - 1
+
+        i = l
+        j = r
+        pivot = arr[i]
+        while i < j:
+            while i < j and pivot < arr[j]:
+                j -= 1
+            if i < j:
+                arr[i] = arr[j]
+                i += 1
+            while i < j and arr[i] < pivot:
+                i += 1
+            if i < j:
+                arr[j] = arr[i]
+                j -= 1
+        arr[i] = pivot
+        self.quicksort(arr[l:i])
+        self.quicksort(arr[i + 1:r + 1])
+        return arr
+
+
+x = Sort()
+print(x.quicksort([5,1,1,2,0,0]))
