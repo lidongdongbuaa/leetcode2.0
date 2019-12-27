5b4dc73faefc9e9d6c27bae012db43e5c47586f0
lidongdong <lidongdongbuaa@gmail.com>
2019-12-27 11:34:35 +0900

    1227

--- test.py
+++ test.py
@@ -1,21 +1,2 @@
-class sortArr:
-    def sortPN(self, arr):  # put the negative front fo positive value
-        if len(arr) == 0 or len(arr) == 1:  # edge case
-            return arr
-
-        i = 0
-        j = len(arr) - 1
-        pivot = arr[i]
-        while i < j:
-            while i < j and arr[j] >= 0:
-                j -= 1
-            arr[i] = arr[j]
-            while i < j and arr[i] < 0:
-                i += 1
-            arr[j] = arr[i]
-        arr[i] = pivot
-        return arr
-
-
-x = sortArr()
-print(x.sortPN([9, 8, 7, 4, 5, -1, -2, -3]))
+a = [0]*10
+print(a)
