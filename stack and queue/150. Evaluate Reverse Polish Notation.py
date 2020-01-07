3ca07538ec32cb0bde8e3f1d3f68e9158f7a308a
lidongdong <lidongdongbuaa@gmail.com>
2020-01-07 11:41:21 +0900

    0107

---
+++ stack and queue/150. Evaluate Reverse Polish Notation.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/7 8:10
+# @Author  : LI Dongdong
+# @FileName: 150. Evaluate Reverse Polish Notation.py.py
+''''''
+'''
+题目分析
+1.要求：Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+    Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+    Note:
+        Division between two integers should truncate toward zero.两个整数之间的除法应截断为零，即取整，但-1/2 = 0
+        The given RPN expression is always valid. That means the expression would always evaluate to a result and there won't be any divide by zero operation.
+    
+    Example 1:
+    Input: ["2", "1", "+", "3", "*"]
+    Output: 9
+    Explanation: ((2 + 1) * 3) = 9
+    
+    Example 2:
+    Input: ["4", "13", "5", "/", "+"]
+    Output: 6
+    Explanation: (4 + (13 / 5)) = 6
+2.理解：
+3.类型：
+4.方法及方法分析：
+time complexity order: 
+space complexity order: 
+'''
+'''
+思路：
+方法：
+边界条件：
+time complex: 
+space complex: 
+易错点：
+'''
