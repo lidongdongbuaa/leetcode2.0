a5e71963602266b9ff719703aeb540c0628d09dd
lidongdong <lidongdongbuaa@gmail.com>
2020-01-17 11:39:59 +0900

    0117

--- Bit manipulation 位运算/反转相关题目/190. Reverse Bits.py
+++ Bit manipulation 位运算/反转相关题目/190. Reverse Bits.py
@@ -10,17 +10,57 @@
     Input: 00000010100101000001111010011100
     Output: 00111001011110000010100101000000
     Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
-2.理解：
-3.类型：
+2.理解：转置数字的二进制表示
+3.类型：bit
 4.确认输入输出及边界条件：
+    input: int < 2**31 - 1
+    output: int
+    corner case: None？ Y - output None
 4.方法及方法分析：
 time complexity order: 
 space complexity order: 
 '''
 '''
-思路：
+A.
+思路： brute force - bit list and reverse 
 方法：
-time complex: 
-space complex: 
+    1. transfer as binary bit, save in list
+    2. reverse the list
+    3. use the list to rebuild the numb, output
+time complex: O(32) = O(1)
+space complex: O(32) = O(1)
 易错点：
 '''
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        if n == None:
+            return None
+
+        bit = []
+        for i in range(32):  # bit save in list
+            bit.append(n >> i & 1)
+
+        bit.reverse()
+
+        # can't transfer to bit
+
+'''
+B.
+优点：技巧性太强
+思路：change 0 bit based on n
+方法：
+    1. scan n 
+        if ith == 1, change (31-i)th to 1 of res (res = 0)
+time complex: O(32) = O(1)
+space complex: O(1)
+易错点：原来可以通过方法，改变一个数(e.g. 0)的bit位的
+'''
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        if n == None:  # corner case
+            return None
+
+        res = 0
+        for i in range(32):
+            if n >> i & 1 == 1:
+                res >> (32 - i)
--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-
-class Solution:
-    def singleNumber(self, nums) -> int:
-        a = 0
-        b = 0
-        for elem in nums:
-            b = (b ^ elem) & ~a
-            a = (a ^ elem) & ~b
-        return b
-
-x = Solution()
-print(x.singleNumber([0,1,0,1,0,1,99]))
+a = 2
+for i in range(32):
+    print( a >> i & 1)
