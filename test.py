94397c7b6b68852d0f05676382451338c3b61245
LI Dongdong <lidongdongbuaa@gmail.com>
2020-01-30 20:12:02 +0900

    0130

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-maxD = [[1,4,1,40,10],[2,8,0,50,5],[3,8,1,30,4],[4,10,0,10,3],[5,1,1,15,1]]
-maxD.sort(key = lambda x: (x[1], x[0]), reverse= True )
-print(maxD)
+def enroll(name, gender, age=6, city='Beijing'):
+    print('name:', name)
+    print('gender:', gender)
+    print('age:', age)
+    print('city:', city)
+
+enroll('li', 'male', 6)
