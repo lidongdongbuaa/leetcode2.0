af607f26bc187b4f2189aec4bdafeeac97ceecb3
lidongdong <lidongdongbuaa@gmail.com>
2020-04-03 22:43:35 +0900

    0403

--- test.py
+++ test.py
@@ -1,66 +1,30 @@
-'''
-矩阵
-求矩阵一个角到另一个角的所有路径
+class Solution:
+    def letterCombinations(self, digits: str):
+        tag = {'2': ['a', 'b', 'c'], '3': ['d', 'e', 'f'], '4': ['g', 'h', 'i']}
 
-A.backtrack - template 1
-易错点， x是小于m，不是小于m - 1
-'''
-class Matrix:
-    def findPath(self, matrix):  # return all path from [0][0] to [-1][-1]
-        if not matrix or matrix == [[]]:  # corner case
-            return []
+        strList = [tag[elem] for elem in digits]
 
-        m, n = len(matrix), len(matrix[0])
-        visited = [[0] * n for _ in range(m)]
+        m = len(digits)
 
-        def backtrack(i, j, path):  # save path in allPath
-            for (x, y) in ((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)):
-                if 0 <= x < m  and 0 <= y < n and visited[x][y] == 0:
-                    path.append(matrix[x][y])
-                    if x == m - 1 and y == n - 1:
-                        allPath.append(path[:])
-                    visited[i][j] = 1
-                    backtrack(x, y, path)
-                    path.pop()
-                    visited[i][j] = 0
-
-        allPath = []
-        visited[0][0] = 1
-        backtrack(0, 0, [matrix[0][0]])
-        return allPath
-
-
-'''
-B. backtrack - template 2 from root to backtrack
-判断条件别忘visitd
-'''
-class Matrix:
-    def findPath(self, matrix):
-        if not matrix or matrix == [[]]:
-            return []
-
-        m, n = len(matrix), len(matrix[0])
-        visited = [[0] * n for _ in range(m)]
-
-        def backtrack(i, j, path): # save all path in allPath
-            if i > m - 1 or i < 0 or j < 0 or j > n - 1 or visited[i][j] == 1:
+        def backtrack(start, path):  # save all combinations in res
+            # if start == m:
+            #     res.append(path[:])
+            #     return 
+            if start > m - 1:
                 return
 
-            path.append(matrix[i][j])
-            visited[i][j] = 1
+            for i in range(len(strList[start])):
+                path.append(strList[start][i])
+                if len(path) == m:
+                    res.append(path[:])
 
-            if i == m - 1 and j == n - 1:
-                allPath.append(path[:])
+                backtrack(start + 1, path)
+                path.pop()
 
-            for (x, y) in ((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)):
-                backtrack(x, y, path)
+        res = []
+        backtrack(0, [])
+        res = [''.join(elem) for elem in res]
+        return res
 
-            path.pop()
-            visited[i][j] = 0
-
-        allPath = []
-        backtrack(0, 0, [])
-        return allPath
-
-x = Matrix()
-print(x.findPath([[0,1,2],[3,4,5]]))
+x = Solution()
+print(x.letterCombinations("23"))
