4c9e94d09328b05059b9e5ca9108630396475df9
lidongdong <lidongdongbuaa@gmail.com>
2020-04-15 07:26:52 +0900

    0415

--- test3.py
+++ test3.py
@@ -1,6 +1,3 @@
-n = 5
+y = [0, 2]
 
-for l in range(2, n + 1):
-    for i in range(n - l + 1):
-        j = l + i - 1
-        print((i,j))
+print(y[:-1])
--- 回溯法/46. Permutations.py
+++ 回溯法/46. Permutations.py
@@ -14,21 +14,44 @@ space complexity order:
 如何考
 '''
 
+'''
+input: list; lenght range of nums is from 0 to inf; not repeat; no order
+output: list[list]
+corner case
+    nums is None, return []
+    nums is only one element, return [[nums[0]]]
+
+A. bracktracking
+    Method:
+        1. corner case
+        2. use recursion and backtracking to push and pop the candidates
+    Time complexity: O(2^n), n is length of nums
+    Space: O(2^n)
+'''
+
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        if not nums:  # corner case
+            return []
+        if len(nums) == 1:
+            return [[nums[0]]]
+
         res = []
 
-        def backtrack(path, nums):
+        n = len(nums)
 
-            for i in nums:
-                if i in path:
+        def backtrack(i, path):  # save all candidates in res list
+            if i == n:
+                res.append(path[:])
+                return
+
+            for x in nums:
+                if x in path:
                     continue
-                path.append(i)
-                if len(path) == len(nums):
-                    res.append(path[:])
-                backtrack(path, nums)
+                path.append(x)
+                backtrack(i + 1, path)
                 path.pop()
 
-        backtrack([], nums)
+        backtrack(0, [])
         return res
