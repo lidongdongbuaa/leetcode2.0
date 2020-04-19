25ec08ca21e7863bf8896d283c3e558d55f7aa02
lidongdong <lidongdongbuaa@gmail.com>
2020-04-19 19:59:49 +0900

    0419

--- test2.py
+++ test2.py
@@ -1,28 +1,33 @@
+from collections import defaultdict
+
+
 class Solution:
-    def maximalSquare(self, matrix) -> int:
-        if not matrix or matrix == [[]]:  # corner case
-            return 0
+    def displayTable(self, orders):
+        food = []
+        table = []
+        for elem in orders:
+            if elem[2] not in food:
+                food.append(elem[2])
+            if elem[1] not in table:
+                table.append(elem[1])
 
-        m, n = len(matrix), len(matrix[0])
+        food.sort()
+        table.sort(key = lambda x : int(x))
 
-        dp = [[float('-inf')] * (n + 1) for _ in range(m + 1)]
+        res = []
+        res.append(['Table'] + food)
 
-        maxLength = 0
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if i == 1 and j == 1:
-                    if matrix[i - 1][j - 1] == '1':
-                        dp[i][j] = 1
-                    else:
-                        dp[i][j] = float('-inf')
-                else:
-                    dp[i][j] = min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1
-
-                if dp[i][j] != float('-inf'):
-                    maxLength = max(maxLength, dp[i][j])
-
-        return maxLength * maxLength
+        for elem in table:
+            row = []
+            row.append(elem)
+            row.extend(['0' for _ in range(len(food))])
+            for menu in orders:
+                if elem == menu[1]:
+                    ind = food.index(menu[2])
+                    row[ind + 1] = str(int(row[ind + 1]) + 1)
+            res.append(row)
+        return res
 
 
 x = Solution()
-print(x.maximalSquare([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]))
+print(x.displayTable([["David","3","Ceviche"],["Corina","10","Beef Burrito"],["David","3","Fried Chicken"],["Carla","5","Water"],["Carla","5","Ceviche"],["Rous","3","Ceviche"]]))
