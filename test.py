5d21c1b5439cec76f20bb335c815c0d0c089595e
lidongdong <lidongdongbuaa@gmail.com>
2020-05-02 09:00:08 +0900

    0501

--- test.py
+++ test.py
@@ -1,21 +1,4 @@
-class Solution:
-    def canJump(self, nums) -> bool:
-        farIndex = [ind + val for ind, val in enumerate(nums)]
-
-        end = farIndex[0]
-        n = len(farIndex)
-        i = 0
-        while i <= end and i <= n - 1:
-            if farIndex[i] > end:
-                end = farIndex[i]
-            else:
-                i += 1
-        if i == n:
-            return True
-        else:
-            return False
-
-
-X = Solution()
-print(X.canJump([2,3,1,1,4]))
-
+from heapq import *
+l = [1,3,4,2]
+r = heapify(l)
+print(r)
