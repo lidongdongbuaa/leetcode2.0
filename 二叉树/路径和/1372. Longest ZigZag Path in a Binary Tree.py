7e5d7a173ceda9be1d46223869471b783f868fe9
lidongdong <lidongdongbuaa@gmail.com>
2020-05-25 09:17:39 +0900

    0525

--- 二叉树/路径和/1372. Longest ZigZag Path in a Binary Tree.py
+++ 二叉树/路径和/1372. Longest ZigZag Path in a Binary Tree.py
@@ -41,8 +41,6 @@ Steps:
             res = max(res, cur)
         3. return res
 '''
-
-
 class Solution:
     def longestZigZag(self, root: TreeNode) -> int:
         if not root:
@@ -53,27 +51,17 @@ class Solution:
         self.res = 0
 
         def dfs(root):  # return left and right longest zigzag path
-            if not root.left and not root.right:
-                return 0, 0
+            if not root:
+                return -1, -1
 
-            leftL, leftR, rightL, rightR = 0, 0, 0, 0
-            if root.left:
-                leftL, leftR = dfs(root.left)
-            if root.right:
-                rightL, rightR = dfs(root.right)
+            leftL, leftR = dfs(root.left)
+            rightL, rightR = dfs(root.right)
             cur = max(leftR, rightL) + 1
             self.res = max(self.res, cur)
-
-            if not root.left:
-                return 0, rightL + 1
-            if not root.right:
-                return leftR + 1, 0
-            else:
-                return leftR + 1, rightL + 1
+            return leftR + 1, rightL + 1
 
         dfs(root)
         return self.res
-
 
 '''
 多参数方法
@@ -100,3 +88,36 @@ class Solution:
 
         dfs(root, 0, 0)
         return self.res
+
+
+'''
+bfs
+steps:
+1. queue = [root, l, r], reach root, the longest path from left or right
+2. pop node from queue, renew the res
+3. push root.left and root.right into the queue
+4. return the res
+Time complexity: O(n)
+Space: O(1)
+'''
+
+
+class Solution:
+    def longestZigZag(self, root: TreeNode) -> int:
+        if not root:  # corner case
+            return -1
+        if not root.left and not root.right:
+            return 0
+
+        from collections import deque
+        queue = deque([(root, 0, 0)])
+
+        res = 0
+        while queue:
+            root, l, r = queue.popleft()
+            res = max(res, l, r)
+            if root.left:
+                queue.append([root.left, 0, l + 1])
+            if root.right:
+                queue.append([root.right, r + 1, 0])
+        return res
